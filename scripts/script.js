7a8653e4293c1c1080d85c27f10afe283229c4c7
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-22 22:44:58 +0300

    Testing the outcome

--- scripts/script.js
+++ scripts/script.js
@@ -11,22 +11,28 @@ var yellowAudio = new Audio('./assets/sounds/yellow.mp3');
 var blueAudio = new Audio('./assets/sounds/blue.mp3');
 var wrongAudio = new Audio('./assets/sounds/wrong.mp3');
 
-let maxlevel=window.prompt("Enter the number of levels please");
+
+//let maxlevel=parseInt(window.prompt("Enter the number of levels please")) ;
+
+var maxlevel = 10;
 var pattern=[];
-var flagStarted=true;
+var flagStarted=false;
+var square=0;
 var sequence=[];
-document.addEventListener("keypress",wrongAnimation);
+var level=0;
+var levelCounter=0;
 function clickGreen (){
-    if (flagStarted===true){
+    if (flagStarted=true){
         greensquare.style.backgroundColor="gray";
         greenAudio.play();
+        console.log("green")
         setTimeout(function(){
             greensquare.style.backgroundColor="green";
         },100);
     }
 }
 function clickRed (){
-    if (flagStarted===true){
+    if (flagStarted=true){
         redsquare.style.backgroundColor="gray";
         redAudio.play();
         setTimeout(function(){
@@ -35,7 +41,7 @@ function clickRed (){
     }
 }
 function clickBlue (){
-    if (flagStarted===true){
+    if (flagStarted=true){
         bluesquare.style.backgroundColor="gray";
         blueAudio.play();
         setTimeout(function(){
@@ -44,7 +50,7 @@ function clickBlue (){
     }
 }
 function clickYellow (){
-    if (flagStarted===true){
+    if (flagStarted=true){
         yellowsquare.style.backgroundColor="gray";
         yellowAudio.play();
         setTimeout(function(){
@@ -59,3 +65,134 @@ function wrongAnimation (){
         mainContainer.style.backgroundColor="navy";
     },300)
 }
+
+function startSequence(){
+    sequence=[];
+    level++;
+    flagStarted=false;
+    
+    if (level<maxlevel){
+        square = Math.floor(Math.random() * 4) + 1;
+        gameText.innerHTML = "Level " + `${level}`;
+        if (square==1){
+            clickGreen();
+            console.log(square)
+        }
+        else if (square==2){
+            clickRed();
+        }
+        else if (square==3){
+            clickBlue();
+        }
+        else if (square==4){
+            clickYellow();
+        }
+        pattern.push(square);
+        console.log("pat",pattern);
+        console.log("seq",sequence);
+    } else {
+        gameText.innerHTML="You won! Congrats!";
+        setTimeout(function(){
+            window.location.reload();},1000)
+    }
+    flagStarted=true;
+    levelCounter=0;
+   
+}
+greensquare.addEventListener("click",greenClicked);
+redsquare.addEventListener("click",redClicked);
+bluesquare.addEventListener("click",blueClicked);
+yellowsquare.addEventListener("click",yellowClicked);
+document.addEventListener("keypress", startSequence);
+
+
+function greenClicked (){
+    console.log("greenclicked");
+    console.log("seq",sequence);
+    console.log("pat",pattern);
+    clickGreen();
+    sequence.push(1);
+    for (var i=0;i<sequence.length;i++){
+        console.log("seq",sequence);
+       
+        if (sequence[i]!=pattern[i]){
+            wrongAnimation();
+            gameText.innerHTML="Game Over, Press Any Key to Restart";
+            setTimeout(function () { location.reload(); }, 300);
+            return;
+        }
+    }
+    levelCounter++;
+    if (levelCounter==level){
+        setTimeout(function(){
+            startSequence();
+        },1000)
+    }
+}
+function redClicked (){
+    console.log("seq",sequence);
+    console.log("pat",pattern);
+    clickRed();
+    sequence.push(2);
+    for (var i=0;i<sequence.length;i++){
+        if (sequence[i]!=pattern[i]){
+            wrongAnimation();
+            gameText.innerHTML="Game Over, Press Any Key to Restart";
+            setTimeout(function () { location.reload(); }, 300);
+            return;
+        }
+
+    }
+    levelCounter++;
+    if (levelCounter==level){
+        setTimeout(function(){
+            startSequence();
+        },1000)
+    }
+}
+function blueClicked (){
+    console.log("seq",sequence);
+    console.log("pat",pattern);
+    clickBlue();
+    sequence.push(3);
+    for (var i=0;i<sequence.length;i++){
+        if (sequence[i]!=pattern[i]){
+            wrongAnimation();
+            gameText.innerHTML="Game Over, Press Any Key to Restart";
+            setTimeout(function () { location.reload(); }, 300);
+            return;
+        }
+
+    }
+    levelCounter++;
+    if (levelCounter==level){
+        setTimeout(function(){
+            startSequence();
+        },1000)
+    }
+}
+function yellowClicked (){
+    console.log("seq",sequence);
+    console.log("pat",pattern);
+    clickYellow();
+    sequence.push(4);
+    for (var i=0;i<sequence.length;i++){
+        if (sequence[i]!=pattern[i]){
+            wrongAnimation();
+            gameText.innerHTML="Game Over, Press Any Key to Restart";
+            setTimeout(function () { location.reload(); }, 300);
+            return;
+        }
+
+    }
+    levelCounter++;
+    if (levelCounter==level){
+        setTimeout(function(){
+            startSequence();
+        },1000)
+    }
+}
+
+
+
+
